511b5b42968300b0343cb5c15eab4d48284a78fd
MathiasLeleu <mathiasleleupro@gmail.com>
2025-04-22 14:26:19 +0200

    feat: Difficulty model

---
+++ src/models/Difficulty.js
@@ -0,0 +1,22 @@
+import { sequelize } from "./connection";
+import { Model, DataTypes } from sequelize
+
+export class Difficulty extends Model {}
+
+Difficulty.init(
+    {
+        name: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+            unique: true,
+        },
+        color: {
+            type: DataTypes.TEXT,
+            defaultValue: '#ffffff',
+        },
+    },
+    {
+        sequelize,
+        tableName: 'difficulty'
+    }
+);
