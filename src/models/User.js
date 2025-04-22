3b149cd7b01a109d198abf0a496191f5ebefc645
kishankarunanithy <jeya-kishan.karunanithy@oclock.school>
2025-04-22 14:50:05 +0200

    models/user.js

---
+++ src/models/User.js
@@ -0,0 +1,39 @@
+import { Model, DataTypes } from "sequelize";
+import { sequelize } from "./connection.js"; 
+
+export class User extends Model {}
+
+User.init(
+  {
+    pseudo: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+      unique: true,
+    },
+    email: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    avatar: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    role: {
+      type: DataTypes.ENUM("user", "admin"),
+      allowNull: false,
+      defaultValue: "user",
+    },
+  },
+  {
+    sequelize,
+    tableName: "user",
+  }
+);
