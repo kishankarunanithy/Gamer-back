719393077853e573808bf39250e3b225e71a11b9
MathiasLeleu <mathiasleleupro@gmail.com>
2025-04-23 13:47:53 +0200

    getAllDifficulties and getOneDifficulty routers DONE

--- src/controllers/difficultyController.js
+++ src/controllers/difficultyController.js
@@ -2,6 +2,31 @@ import { User, Challenge, Category, Difficulty, sequelize } from "../models/asso
 
 const difficultyController = {
     
+    async showAllDifficulties(req, res) {
+        res.status(200).json(await Difficulty.findAll());
+    },
+
+    async showOneDifficulty(req, res, next) {
+        const result = await Difficulty.findByPk(req.params.id);
+
+        if (!result) {
+            return next();
+        }
+
+        res.status(200).json(result);
+    },
+
+    async createDifficulty() {
+
+    },
+
+    async updateDifficulty() {
+
+    },
+
+    async deleteDifficulty() {
+
+    }
 };
 
 export { difficultyController };
--- src/router.js
+++ src/router.js
@@ -1,4 +1,5 @@
 import { Router } from 'express'
+import { cw } from './middlewares/controllerWrapper.js';
 
 import { difficultyController } from './controllers/difficultyController.js';
 
@@ -6,10 +7,10 @@ const router = Router();
 
 router.get("/", (req, res ) => {res.send("Hello les G@MERS")})
 
-router.get("/difficulties", cw(difficultyController.));
-router.get("/difficulties/:id", cw(difficultyController.));
-router.post("/difficulties", cw(difficultyController.));
-router.patch("/difficulties/:id", cw(difficultyController.));
-router.delete("/difficulties/:id", cw(difficultyController.));
+router.get("/difficulties", cw(difficultyController.showAllDifficulties));
+router.get("/difficulties/:id", cw(difficultyController.showOneDifficulty));
+//router.post("/difficulties", cw(difficultyController.));
+//router.patch("/difficulties/:id", cw(difficultyController.));
+//router.delete("/difficulties/:id", cw(difficultyController.));
 
 export { router }
