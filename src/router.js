45f8343823e83bc6650b1b3f316909e0aa5c4e92
kishankarunanithy <jeya-kishan.karunanithy@oclock.school>
2025-04-23 14:15:55 +0200

    add controller file and change router for challege

--- src/router.js
+++ src/router.js
@@ -1,6 +1,15 @@
 import { Router } from 'express'
-
+import { challengeController } from "./controllers/challengeContoller";
+import { cw } from "../middlewares/controllerWrapper.js";
 const router = Router();
+
+// Routes pour les challenges
+router.get("/challenges", cw(challengeController.showAllChallenges));
+router.get("/challenges/:id", cw(challengeController.showOneChallenge));
+router.post("/challenges", cw(challengeController.createChallenge));
+router.patch("/challenges/:id", cw(challengeController.updateChallenge));
+router.delete("/challenges/:id", cw(challengeController.deleteChallenge));
+
 
 router.get("/", (req, res ) => {res.send("Hello les G@MERS")})
 
