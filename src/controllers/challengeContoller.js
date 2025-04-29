367c662eb2944bf3bac6298cf0731afbddf1c88c
SandrineLJ <sandrine.louisjean@gmail.com>
2025-04-29 14:21:09 +0200

    fix: add associations to showOneChallenge function

--- src/controllers/challengeContoller.js
+++ src/controllers/challengeContoller.js
@@ -2,74 +2,82 @@ import { Challenge, User, Category, Difficulty } from "../models/association.js"
 import { notFound } from '../utils/error.js';
 
 const challengeController = {
-  // récupère tous les challenges + includes
-  async showAllChallenges(req, res) {
-    const result = await Challenge.findAll({
-        include: [
-          { association: "users",
-            attributes: { exclude: ["password"] }
-          },
-          { association: "category" },
-          { association: "difficulty" }
-        ],
-      });
-    res.status(200).json(result);
-  },
+	// récupère tous les challenges + includes
+	async showAllChallenges(req, res) {
+		const result = await Challenge.findAll({
+			include: [
+			{ association: "users",
+				attributes: { exclude: ["password"] }
+			},
+			{ association: "category" },
+			{ association: "difficulty" }
+			],
+		});
+		res.status(200).json(result);
+	},
 
-  // récupère un challenge avec son id
-  async showOneChallenge(req, res, next) {
-    const result = await Challenge.findByPk(req.params.id);
+	// récupère un challenge avec son id
+	async showOneChallenge(req, res, next) {
+		const result = await Challenge.findByPk(req.params.id, {
+		include: [
+			{ association: "users",
+			attributes: { exclude: ["password"] }
+			},
+			{ association: "category" },
+			{ association: "difficulty" }
+		]
+		});
 
-    if (!result) {
-      notFound(`Catégorie avec l'ID ${req.params.id} non trouvée`);
-  }
+		if (!result) {
+			notFound(`Catégorie avec l'ID ${req.params.id} non trouvée`);
+		}
 
-    res.status(200).json(result);
-  },
+		res.status(200).json(result);
+	},
 
-  // crée un challenge
-  async createChallenge(req, res) {
-    const { name, description, video_url, user_id, category_id, difficulty_id } = req.body;
+	// crée un challenge
+	async createChallenge(req, res) {
+		const { name, description, video_url, user_id, category_id, difficulty_id } = req.body;
 
-    
-    if (!name) return res.status(400).json({ message: "Le nom est requis." });
-    if (!description) return res.status(400).json({ message: "La description est requise." });
-    if (!video_url) return res.status(400).json({ message: "Le lien vidéo est requis." });
-    if (!user_id) return res.status(400).json({ message: "user_id est requis." });
-    if (!category_id) return res.status(400).json({ message: "category_id est requis." });
-    if (!difficulty_id) return res.status(400).json({ message: "difficulty_id est requis." });
+		
+		if (!name) return res.status(400).json({ message: "Le nom est requis." });
+		if (!description) return res.status(400).json({ message: "La description est requise." });
+		if (!video_url) return res.status(400).json({ message: "Le lien vidéo est requis." });
+		if (!user_id) return res.status(400).json({ message: "user_id est requis." });
+		if (!category_id) return res.status(400).json({ message: "category_id est requis." });
+		if (!difficulty_id) return res.status(400).json({ message: "difficulty_id est requis." });
 
-    // on crée le challenge
-    const result = await Challenge.create({
-      name,
-      description,
-      video_url,
-      user_id,
-      category_id,
-      difficulty_id
-    });
+		// on crée le challenge
+		const result = await Challenge.create({
+		name,
+		description,
+		video_url,
+		user_id,
+		category_id,
+		difficulty_id
+		});
 
-    res.status(201).json(result);
-  },
+		res.status(201).json(result);
+	},
 
-  // modifie un challenge
-  async updateChallenge(req, res) {
-    const result = await Challenge.update(req.body, {
-      where: { id: req.params.id }
-    });
+	// modifie un challenge
+	async updateChallenge(req, res) {
+		const result = await Challenge.update(req.body, {
+			where: { id: req.params.id }
+		});
 
-    if (!result) {
-      notFound(`Catégorie avec l'ID ${req.params.id} non trouvée`);
-    }
+		if (!result) {
+			notFound(`Catégorie avec l'ID ${req.params.id} non trouvée`);
+		}
 
-    res.status(200).json({ message: "Challenge modifié", result });
-  },
+		res.status(200).json({ message: "Challenge modifié", result });
+	},
 
-  // supprime un challenge
-  async deleteChallenge(req, res) {
-    await Challenge.destroy({ where: { id: req.params.id } });
-    res.status(204).send();
-  }
+	// supprime un challenge
+	async deleteChallenge(req, res) {
+		await Challenge.destroy({ where: { id: req.params.id } });
+		res.status(204).send();
+	}
 };
 
 export { challengeController };
