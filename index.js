aa0f44360784adb8148b1cdf66502aece6f7e5ff
kishankarunanithy <jeya-kishan.karunanithy@oclock.school>
2025-05-15 11:00:24 +0200

    change cors

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.set("trust proxy", 1);
 app.use(express.json());
 
 app.use(cors({
-    origin: ["http://localhost:5173"]
+    origin: ["https://gamer-front.vercel.app/"]
 }));
 
 app.get("/", (req, res) => {
