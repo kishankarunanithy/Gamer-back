f9341671e5ecb208f6180fcc717fba62f2af1290
kishankarunanithy <jeya-kishan.karunanithy@oclock.school>
2025-05-19 10:03:17 +0200

    db:reset restart

--- src/models/connection.js
+++ src/models/connection.js
@@ -2,6 +2,7 @@ import "dotenv/config"
 import { Sequelize } from 'sequelize';
 console.log("ENV :", process.env.NODE_ENV);
 const isProduction = process.env.NODE_ENV === 'production';
+console.log("Connecting to:", process.env.DB_URL);
 
 const sequelize = new Sequelize(process.env.DB_URL, {
   dialect: 'postgres',
