1e354a569fd80aa06e9a1394c278e4486a84e742
kishankarunanithy <jeya-kishan.karunanithy@oclock.school>
2025-05-19 09:49:26 +0200

    try again db:reset

--- src/models/connection.js
+++ src/models/connection.js
@@ -1,19 +1,17 @@
-// Dans src/models/connection.js
-import 'dotenv/config';
+import "dotenv/config"
 import { Sequelize } from 'sequelize';
-
 console.log("ENV :", process.env.NODE_ENV);
 const isProduction = process.env.NODE_ENV === 'production';
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const sequelize = new Sequelize(process.env.DB_URL, {
   dialect: 'postgres',
-  dialectOptions: isProduction
-    ? {
-        ssl: {
-          require: true,
-          rejectUnauthorized: false, // À utiliser avec prudence en production
-        },
-      }
+  dialectOptions: isProduction 
+   ? {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    }
     : false,
 });
 
